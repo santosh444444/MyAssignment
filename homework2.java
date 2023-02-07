35b6362592cfb69308a00e557acf2a26e8ba99c1
santosh444444 <124717858+santosh444444@users.noreply.github.com>
2023-02-07 14:31:22 -0500

    Add files via upload

---
+++ homework2.java
@@ -0,0 +1,19 @@
+public class homework2 {
+    public static void main(String[] args) {
+
+    }        Interst Interst01 = new Interst();
+
+    }
+ class  Interst {
+     int Principlevalue = 30000;
+     int RateOfInt = 4;
+     float year = 7;
+
+     float CompoundInt = (Principlevalue * (1 + RateOfInt / 100) * year);
+
+     void CalculateCompoundIn() {
+         System.out.println("compound year =" + CompoundInt);
+     }
+ }
+
+
