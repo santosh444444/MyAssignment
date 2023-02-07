9a8ce0044f0b3f1904a055024ae3de58bafd5dab
santosh444444 <124717858+santosh444444@users.noreply.github.com>
2023-02-07 16:07:20 -0500

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,15 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+
+            int number=30;
+            System.out.print("List of even numbers from 3 to "+number+": ");
+            for (int i=1; i<=number; i++)
+            {
+                if (i%2==0)
+                {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
