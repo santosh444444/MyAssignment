840b7006be56c877064c991167339de6a77137b7
santosh444444 <124717858+santosh444444@users.noreply.github.com>
2023-02-07 15:42:42 -0500

    Add files via upload

---
+++ DataTypeTest.java
@@ -0,0 +1,27 @@
+public class DataTypeTest {
+    public static void main(String[] args) {
+
+    }        class Travel {
+            void number0fdays() {
+                System.out.println("Travelling for 10 days");
+            }
+
+            void numberOfDays1(int days) {
+                System.out.println("Travelling for"+ days);
+            }
+
+            void number0fDays2(int days, String country) {
+                System.out.println("Travelling for " + days + "days to" + country);
+
+
+            }
+
+            String countryName() {
+                String name1 = "Traveling to Greece";
+                return name1;
+            }
+        }
+    }
+
+
+
